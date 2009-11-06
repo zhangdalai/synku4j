55846d38380eedbd9f3df3ad2c12ce944b969718
joolski <joolski@6d97c07a-caba-11de-ba9f-01e9bd544a36>
2009-11-06 11:01:41 +0000

    Updated Copyright message

--- trunk/org.synku4j.wbxml/src/main/java/org/synku4j/wbxml/WbxmlConstants.java
+++ trunk/org.synku4j.wbxml/src/main/java/org/synku4j/wbxml/WbxmlConstants.java
@@ -1,6 +1,24 @@
+/*
+ * Copyright (C) 2009 Jools Enticknap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
 package org.synku4j.wbxml;
 
-public interface WbxmlConstants {             // 12345678
+public interface WbxmlConstants {        // 12345678
 	static final int SWITCH_PAGE = 0x00; // 00000000
 	static final int END         = 0x01; // 00000001
 	static final int ENTITY      = 0x02; // 00000010
