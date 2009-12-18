c91b962b4b33033ae244e21fb7b918d70921d465
joolski <joolski@6d97c07a-caba-11de-ba9f-01e9bd544a36>
2009-12-18 17:45:12 +0000

    Updated java doc.

--- wbxml/org.synku4j.wbxml.marshal/src/main/java/org/synku4j/wbxml/marshal/WbxmlMarshaller.java
+++ wbxml/org.synku4j.wbxml.marshal/src/main/java/org/synku4j/wbxml/marshal/WbxmlMarshaller.java
@@ -22,10 +22,39 @@ import java.io.OutputStream;
 
 import org.synku4j.wbxml.core.context.WbxmlContext;
 
+/**
+ * The <code>WbxmlMarshaller</code> interface 
+ * 
+ * @author Jools Enticknap
+ */
 public interface WbxmlMarshaller {
+	
+	/**
+	 * Unmarshal from the supplied stream to an object represented by <T>.
+	 * 
+	 * @param <T> return type.
+	 * @param cntx marshaling context. 
+	 * @param is input stream to read from.
+	 * @param t the type to create.
+	 * @param filter filters used to include/exclude fields
+	 * @return an unmarshaled instance of <T>
+	 * @throws IOException thrown via the input stream.
+	 * @throws WbxmlMarshallerException thrown if the stream is not well formed.
+	 */
 	<T> T unmarshal(WbxmlContext cntx, InputStream is, Class<T> t,  String...filter) 
 	throws IOException, WbxmlMarshallerException;
 	
+	/**
+	 * Marshal to the output stream the supplied object as a wbxml stream.
+	 * 
+	 * @param <T> the type to marshal.
+	 * @param cntx marshaling context.
+	 * @param os the output stream to write to.
+	 * @param t the object to marshal.
+	 * @param filter filters used to include/exclude fields
+	 * @throws IOException thrown via the input stream.
+	 * @throws WbxmlMarshallerException thrown if the stream is not well formed.
+	 */
 	<T> void marshal(WbxmlContext cntx, OutputStream os,  T t, String...filter) 
 	throws IOException, WbxmlMarshallerException;
 }
